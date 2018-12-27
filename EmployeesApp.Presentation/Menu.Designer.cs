7a79d6d74d22ad1015e81d77da707e0245d8f1fe
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2018-12-27 16:10:04 +0100

    Created Menu form

--- EmployeesApp.Presentation/Menu.Designer.cs
+++ EmployeesApp.Presentation/Menu.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuLabel = new System.Windows.Forms.Label();
+            this.employeesListButton = new System.Windows.Forms.Button();
+            this.employeeCreateButton = new System.Windows.Forms.Button();
+            this.projectsListButton = new System.Windows.Forms.Button();
+            this.projectCreateButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.EmployeesGroupBox = new System.Windows.Forms.GroupBox();
+            this.projectsGroupBox = new System.Windows.Forms.GroupBox();
+            this.EmployeesGroupBox.SuspendLayout();
+            this.projectsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuLabel
+            // 
+            this.menuLabel.AutoSize = true;
+            this.menuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuLabel.Location = new System.Drawing.Point(335, 43);
+            this.menuLabel.Name = "menuLabel";
+            this.menuLabel.Size = new System.Drawing.Size(143, 48);
+            this.menuLabel.TabIndex = 0;
+            this.menuLabel.Text = "MENU";
+            // 
+            // employeesListButton
+            // 
+            this.employeesListButton.Location = new System.Drawing.Point(67, 37);
+            this.employeesListButton.Name = "employeesListButton";
+            this.employeesListButton.Size = new System.Drawing.Size(151, 62);
+            this.employeesListButton.TabIndex = 1;
+            this.employeesListButton.Text = "Popis radnika";
+            this.employeesListButton.UseVisualStyleBackColor = true;
+            // 
+            // employeeCreateButton
+            // 
+            this.employeeCreateButton.Location = new System.Drawing.Point(67, 130);
+            this.employeeCreateButton.Name = "employeeCreateButton";
+            this.employeeCreateButton.Size = new System.Drawing.Size(151, 62);
+            this.employeeCreateButton.TabIndex = 2;
+            this.employeeCreateButton.Text = "Kreiranje novog radnika";
+            this.employeeCreateButton.UseVisualStyleBackColor = true;
+            // 
+            // projectsListButton
+            // 
+            this.projectsListButton.Location = new System.Drawing.Point(79, 37);
+            this.projectsListButton.Name = "projectsListButton";
+            this.projectsListButton.Size = new System.Drawing.Size(151, 62);
+            this.projectsListButton.TabIndex = 3;
+            this.projectsListButton.Text = "Popis projekata";
+            this.projectsListButton.UseVisualStyleBackColor = true;
+            // 
+            // projectCreateButton
+            // 
+            this.projectCreateButton.Location = new System.Drawing.Point(79, 130);
+            this.projectCreateButton.Name = "projectCreateButton";
+            this.projectCreateButton.Size = new System.Drawing.Size(151, 62);
+            this.projectCreateButton.TabIndex = 4;
+            this.projectCreateButton.Text = "Kreiranje novog projekta";
+            this.projectCreateButton.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(344, 399);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 39);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // EmployeesGroupBox
+            // 
+            this.EmployeesGroupBox.Controls.Add(this.employeesListButton);
+            this.EmployeesGroupBox.Controls.Add(this.employeeCreateButton);
+            this.EmployeesGroupBox.Location = new System.Drawing.Point(74, 132);
+            this.EmployeesGroupBox.Name = "EmployeesGroupBox";
+            this.EmployeesGroupBox.Size = new System.Drawing.Size(302, 222);
+            this.EmployeesGroupBox.TabIndex = 6;
+            this.EmployeesGroupBox.TabStop = false;
+            this.EmployeesGroupBox.Text = "Radnje nad radnicima";
+            // 
+            // projectsGroupBox
+            // 
+            this.projectsGroupBox.Controls.Add(this.projectCreateButton);
+            this.projectsGroupBox.Controls.Add(this.projectsListButton);
+            this.projectsGroupBox.Location = new System.Drawing.Point(435, 132);
+            this.projectsGroupBox.Name = "projectsGroupBox";
+            this.projectsGroupBox.Size = new System.Drawing.Size(301, 222);
+            this.projectsGroupBox.TabIndex = 7;
+            this.projectsGroupBox.TabStop = false;
+            this.projectsGroupBox.Text = "Radnje nad projektima";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.projectsGroupBox);
+            this.Controls.Add(this.EmployeesGroupBox);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.menuLabel);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
+            this.EmployeesGroupBox.ResumeLayout(false);
+            this.projectsGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label menuLabel;
+        private System.Windows.Forms.Button employeesListButton;
+        private System.Windows.Forms.Button employeeCreateButton;
+        private System.Windows.Forms.Button projectsListButton;
+        private System.Windows.Forms.Button projectCreateButton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox EmployeesGroupBox;
+        private System.Windows.Forms.GroupBox projectsGroupBox;
     }
 }
