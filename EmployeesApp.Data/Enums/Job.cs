cc5d9a2f06947154f6d932f8896ccf4bd83451bd
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2018-12-27 16:33:25 +0100

    Created Job enum

---
+++ EmployeesApp.Data/Enums/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Data.Enums
+{
+    public enum Job
+    {
+        programer,
+        dizajner,
+        računovođa
+    }
+}
