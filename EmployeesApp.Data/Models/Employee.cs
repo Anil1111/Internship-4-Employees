31f087df677adbe24f9784993cfdae041856598b
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2018-12-27 17:11:52 +0100

    Created EmployeeRespository class

--- EmployeesApp.Data/Models/Employee.cs
+++ EmployeesApp.Data/Models/Employee.cs
@@ -15,6 +15,8 @@ namespace EmployeesApp.Data.Models
         public string Oib { get; set; }
         public Job Job { get; set; }
 
+        public static List<Employee> AllEmployees { get; set; }
+
         public List<Project> Projects { get; set; }
 
         public Employee(string name, string surname, DateTime dateOfBirth, string oib, Job job, List<Project> projects)
@@ -25,6 +27,8 @@ namespace EmployeesApp.Data.Models
             Oib = oib;
             Job = job;
             Projects = projects;
+
+            AllEmployees.Add(this);
         }
 
         public override string ToString() => $"{Name} {Surname}({Job}), OIB:{Oib}";
---
+++ EmployeesApp.Domain/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Domain.Repositories
+{
+    public class EmployeeRepository
+    {
+        public List<Employee> GetAllEmployees() => Employee.AllEmployees;
+    }
+}
